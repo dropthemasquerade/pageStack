556ba3892e2b3687433dc9499e4054e876b3aabb
dropthemasquerade <dropthemasquerade@gmail.com>
2020-11-17 15:34:48 +0800

    add extends slice of steps

--- src/parser.go
+++ src/parser.go
@@ -36,27 +36,38 @@ func (c *steps) getStep(f string) *steps {
     return c
 }
 
-func generateSteps(){
+func generateSteps(f string) steps{
   var c steps
-  f := "step.yaml"
   c.getStep(f)
 
   extends_step_path_dash := c.Entrance
   extends_step_path := strings.Replace(extends_step_path_dash, "__", "/", 3)
-
-  fmt.Println(c)
-  fmt.Println(extends_step_path)
+  // fmt.Println("extends_step_path -->", c, extends_step_path, extends_step_path_dash)
+  if extends_step_path == "" {
+    return c
+  }
   p := extends_step_path + ".yaml"
+  // fmt.Println("do me")
   // open the extends_path_file
-  c.getStep(p)
-  if c.Entrance == extends_step_path_dash {
-    err := fmt.Sprintf("Recursion Entrance Error: You should never set entrance to yourself at [ %s ]", p)
+  c1  := generateSteps(p)
+  if c1.Entrance == extends_step_path_dash  {
+    err := fmt.Sprintf("Recursion Entrance Error: You should never set entrance to yourself at [ %s ] [%s]", p, c1.Entrance)
     panic(err)
   }
-  fmt.Println(c)
+
+  // fmt.Println(c)
+  // fmt.Println("c2 steps -->, ", c1.Steps)
+  all_steps := append(c1.Steps, c.Steps...)
+  // fmt.Println("all ---> ", all_steps)
+  c.Steps = all_steps
+  // fmt.Println("c.Steps new -->", c.Steps)
+  return c
 
 }
 
 func main() {
-  generateSteps()
+  f := "step.yaml"
+  c := generateSteps(f)
+
+  fmt.Println("c -->", c)
 }
